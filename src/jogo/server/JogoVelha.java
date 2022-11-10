e9421f571f5f8ffa13aa868309b01acad98f2ff3
matheusfc <matheuscorocher@gmail.com>
2022-11-10 18:05:49 -0300

    fix: remove the action of player paint other player square

--- src/jogo/server/JogoVelha.java
+++ src/jogo/server/JogoVelha.java
@@ -11,11 +11,11 @@ import jogo.lib.JogoVelhaServerMessage;
  * @author matheus
  */
 public class JogoVelha {
-
+    
     protected int turn;
     protected int q1, q2, q3, q4, q5, q6, q7, q8, q9;
     protected boolean isGameOver;
-
+    
     public JogoVelha() {
         this.turn = 1;
         this.q1 = 0;
@@ -29,41 +29,75 @@ public class JogoVelha {
         this.q9 = 0;
         this.isGameOver = false;
     }
-
+    
     public JogoVelhaServerMessage execute(int player, int square) {
-        if (this.isYourTurn(player) && !this.isGameOver) {
+        if (this.isYourTurn(player) && !this.isGameOver && !this.hasPlayerColor(square)) {
             this.setSquareColor(player, square);
             int winner = getWinner();
-
+            
             if (this.isThereAnyWinner(winner)) {
                 this.isGameOver = true;
             }
-
+            
             this.goToNextTurn();
             if (this.shouldResetTurn()) {
                 this.resetTurn();
             }
         }
-
+        
         return this.getGameStatus();
     }
-
+    
     private boolean isYourTurn(int player) {
         return this.turn == player;
     }
-
+    
     private void goToNextTurn() {
         this.turn += 1;
     }
-
+    
     private void resetTurn() {
         this.turn = 1;
     }
-
+    
     private boolean shouldResetTurn() {
         return this.turn == 3;
     }
-
+    
+    private boolean hasPlayerColor(int square) {
+        int s = 0;
+        switch (square) {
+            case 1:
+                s = this.q1;
+                break;
+            case 2:
+                s = this.q2;
+                break;
+            case 3:
+                s = this.q3;
+                break;
+            case 4:
+                s = this.q4;
+                break;
+            case 5:
+                s = this.q5;
+                break;
+            case 6:
+                s = this.q6;
+                break;
+            case 7:
+                s = this.q7;
+                break;
+            case 8:
+                s = this.q8;
+                break;
+            case 9:
+                s = this.q9;
+                break;
+        }
+        return s != 0;
+    }
+    
     private void setSquareColor(int player, int square) {
         switch (square) {
             case 1:
@@ -95,10 +129,10 @@ public class JogoVelha {
                 break;
         }
     }
-
+    
     private int getWinner() {
         int winner = 0;
-
+        
         if (this.q1 != 0 && this.q1 == this.q2 && this.q2 == this.q3) {
             winner = this.q1;
         } else if (this.q4 != 0 && this.q4 == this.q5 && this.q5 == this.q6) {
@@ -118,15 +152,15 @@ public class JogoVelha {
         }
         return winner;
     }
-
+    
     private boolean isThereAnyWinner(int winner) {
         return winner != 0;
     }
-
+    
     protected JogoVelhaServerMessage getGameStatus() {
         return new JogoVelhaServerMessage(this.turn, this.q1, this.q2, this.q3,
                 this.q4, this.q5, this.q6,
                 this.q7, this.q8, this.q9);
     }
-
+    
 }
